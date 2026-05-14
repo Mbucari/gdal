5e52718d8f23afb7a53470d3405f09cb63a4d6ba
Michael Bucari-Tovo <mbucari1@gmail.com>
2026-05-14 14:02:10 -0600

    Add C# and Java build directories to PATH variable

--- scripts/setdevenv.bat
+++ scripts/setdevenv.bat
@@ -3,7 +3,7 @@
 REM This is a very primitive script which must be called from a build
 REM directory and set the environment for a Release build
 
-set PATH=%CD%\swig\python\bin;%CD%\apps\Release;%CD%\Release;%PATH%
+set PATH=%CD%\swig\java\Release;%CD%\swig\csharp\Release;%CD%\swig\python\bin;%CD%\apps\Release;%CD%\Release;%PATH%
 set GDAL_DATA=%CD%\data
 set PYTHONPATH=%CD%\swig\python
 set GDAL_DRIVER_PATH=%CD%\gdalplugins\Release
